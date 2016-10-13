29f2a7b0d79057638fbd7f32d3171feb10da42bd
rohdf <rohdri.frimat@gmail.com>
2016-10-13 15:42:02 +0200

    first commit

---
+++ src/nsis/app/ClientApp.java
@@ -0,0 +1,12 @@
+package nsis.app;
+
+import nsis.socket.Client;
+
+public class ClientApp {
+
+	public static void main(String[] args) {
+		Client client = new Client("192.168.10.208", "rohdri");
+
+	}
+  
+}
